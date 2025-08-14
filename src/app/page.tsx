539dfa05888b52eaa3367317bdbc56de4f645cdc
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-14 18:02:57 +0000

    feat: titulo adicionado na tabela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Plus } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Todo List App</h1>
-      <button className="bg-purple-700 text-white w-full py-2">
-        <p className="font-bold">Nova Tarefa</p>
+    <div className="items-center justify-items-center min-h-screen p-8">
+      <h1 className="font-bold text-4xl mb-6">Todo List App</h1>
+      <button className="bg-purple-700 text-white w-full py-4 mb-4 flex justify-center items-center rounded">
+        <p className="font-bold text-lg mr-2">Nova Tarefa</p>
+        <Plus color="white" />
       </button>
+      <div className="bg-gray-100 w-full p-4 rounded flex justify-between">
+        <h2 className="font-bold text-lg">Tarefas</h2>
+        <h2 className="font-bold text-lg">Ações</h2>
+      </div>
     </div>
   );
 }
